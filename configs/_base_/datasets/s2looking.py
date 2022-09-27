d29d4b354374f30da0312636336ed8fe8dcad85b
likyoo <likyoo@sdust.edu.cn>
2022-09-27 21:12:35 +0800

    upd. some default config

--- configs/_base_/datasets/s2looking.py
+++ configs/_base_/datasets/s2looking.py
@@ -29,7 +29,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=8,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
---
+++ configs/changer/changer_s101_512x512_40k_levircd.py
@@ -0,0 +1,3 @@
+_base_ = './changer_s50_512x512_40k_levircd.py'
+
+model = dict(backbone=dict(depth=101, stem_channels=128))
