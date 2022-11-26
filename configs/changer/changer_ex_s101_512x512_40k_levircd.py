8ffe616c2c48fabdc1b1a213c7fd3b13a668a4b4
likyoo <likyoo@sdust.edu.cn>
2022-11-26 10:20:57 +0800

    fix _base_ ref name

--- configs/changer/changer_ex_s101_512x512_40k_levircd.py
+++ configs/changer/changer_ex_s101_512x512_40k_levircd.py
@@ -1,3 +1,3 @@
-_base_ = './changer_s50_512x512_40k_levircd.py'
+_base_ = './changer_ex_s50_512x512_40k_levircd.py'
 
 model = dict(backbone=dict(depth=101, stem_channels=128))
--- configs/changer/changer_ex_s101_512x512_80k_s2looking.py
+++ configs/changer/changer_ex_s101_512x512_80k_s2looking.py
@@ -1,3 +1,3 @@
-_base_ = './changer_s50_512x512_80k_s2looking.py'
+_base_ = './changer_ex_s50_512x512_80k_s2looking.py'
 
 model = dict(backbone=dict(depth=101, stem_channels=128))
