e4240297f0d6b8902596e2de49868018acdda245
likyoo <likyoo@sdust.edu.cn>
2023-11-28 09:26:44 +0800

    mmcv_maximum_version

--- opencd/__init__.py
+++ opencd/__init__.py
@@ -8,7 +8,7 @@ import mmseg
 from .version import __version__, version_info
 
 mmcv_minimum_version = '2.0.0rc4'
-mmcv_maximum_version = '2.1.0'
+mmcv_maximum_version = '2.2.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 mmengine_minimum_version = '0.6.0'
